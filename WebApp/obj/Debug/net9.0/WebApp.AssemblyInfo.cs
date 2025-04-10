6fe3d88fe53b562b074b6b0fa44fbef0d5838ef2
Johanna Falkenmark <johanna.falkenmark@yh.nackademin.se>
2025-04-10 16:30:59 +0200

    Fixes

--- WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f850772187476fb1162d264f702053b8ca8883a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0bf2e0a55580e619577d233af23d2fa4d43ea4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
