2d31885fa3253c885479ec8c621fe0f0df998092
Johanna Falkenmark <johanna.falkenmark@yh.nackademin.se>
2025-04-11 08:06:28 +0200

    commit

--- WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0bf2e0a55580e619577d233af23d2fa4d43ea4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83ac1a9bd12c80abc8b23238cd98119d9459a8b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
